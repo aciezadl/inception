2d01117f5f387630d4bd6d4b63d4cb5aa3351e8c
Anthony Ciezadlik <aciezadl@paul-f5Ar4s4.clusters.42paris.fr>
2025-03-26 15:48:59 +0100

    govm

--- inception/srcs/requirements/wordpress/conf/auto_config.sh
+++ inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -e
+
 sleep 10
 
-if [ -e /var/www/html/wp-config.php ]
+if [ -e /var/www/wordpress/wp-config.php ]
 then
     echo "Mon fichier existe"
 else
-    wp config create  --allowroot \
+    wp config create  --allow-root \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
         --dbpass=$SQL_PASSWORD \
-        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+        --dbhost="mariadb:3306" --path='/var/www/wordpress'
 fi
+
+mkdir /run/php -p
+
+exec /usr/sbin/php-fpm7.3 -F
