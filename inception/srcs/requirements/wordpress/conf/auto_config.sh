f38944cc5a95ad4c5b1d718c3a94192a3b5c36f2
root <root@aciezadl.42.fr>
2025-03-27 12:39:26 +0100

    pasmal

--- inception/srcs/requirements/wordpress/conf/auto_config.sh
+++ inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-sleep 10
+sleep 8
 
-if [ -e /var/www/wordpress/wp-config.php ]
+if [ ! -f /var/www/wordpress/wp-config.php ]
 then
-    echo "Mon fichier existe"
-else
+    echo "Mon fichier existe pas"
+elif [ ! -s /var/www/wordpress/wp-config.php ]
+then
+    echo "Mon fichier existe mais vide"
+    rm -rf /var/www/wordpress/wp-config.php
     wp config create  --allow-root \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
         --dbpass=$SQL_PASSWORD \
         --dbhost="mariadb:3306" --path='/var/www/wordpress'
+    wp core install --allow-root --url=aciezadl.42.fr --title=inception --admin_user=$WORDPRESS_ADMIN --admin_email=$WORDPRESS_EMAIL_ADMIN --admin_password=$WORDPRESS_PASSWORD_ADMIN 
+    wp user create --allow-root $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD
+else
+    echo "Mon fichier existe remplis"
 fi
 
-mkdir /run/php -p
+# mkdir /run/php -p
 
 exec /usr/sbin/php-fpm7.3 -F
