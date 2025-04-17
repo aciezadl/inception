2a9576e1e0cf398f1fa199cf27aaa31cd41ef8c3
root <root@inception.myguest.virtualbox.org>
2025-04-17 12:14:06 +0200

    finito

--- inception/srcs/requirements/wordpress/conf/auto_config.sh
+++ inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -7,6 +7,13 @@ sleep 8
 if [ ! -f /var/www/wordpress/wp-config.php ]
 then
     echo "Mon fichier existe pas"
+    wp config create  --allow-root \
+        --dbname=$SQL_DATABASE \
+        --dbuser=$SQL_USER \
+        --dbpass=$SQL_PASSWORD \
+        --dbhost="mariadb:3306" --path='/var/www/wordpress'
+    wp core install --allow-root --url=aciezadl.42.fr --title=inception --admin_user=$WORDPRESS_ADMIN --admin_email=$WORDPRESS_EMAIL_ADMIN --admin_password=$WORDPRESS_PASSWORD_ADMIN 
+    wp user create --allow-root $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD
 elif [ ! -s /var/www/wordpress/wp-config.php ]
 then
     echo "Mon fichier existe mais vide"
